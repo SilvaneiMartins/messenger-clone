67e11c68a9bdeb285f7cabcc996b930c51b707ab
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-11 10:17:13 -0300

    Configuração module resolver babel e tsconfig

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            'module-resolver',
+            {
+                root: ['./rsc'],
+                extends: ['.ts', '.tsx', '.js', '.json'],
+                alias: {
+                    'assets': './src/assets',
+                    'components': './src/components',
+                    'hooks': './src/hooks',
+                    'routes': './src/routes',
+                    'screens': './src/screens',
+                    'styles': './src/styles',
+                    'utils': './src/utils',
+                }
+            }
+        ]
+    };
 };
---
+++ src/MyApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MyApp = () => {
+  return (
+    <View>
+      <Text>MyApp</Text>
+    </View>
+  )
+}
+
+export default MyApp
